e02bf2785e8bab94d0ae74006062bfa2cc4eaf16
Yan Hong <onlyhongyan@gmail.com>
2017-02-19 16:33:15 -0800

    implement cors

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var express    = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     mongoose   = require('mongoose'),
-    config = require('./config');
+    config = require('./config'),
+    cors = require('cors');
 
 var port = process.env.PORT || 8080;
 mongoose.connect(config.database); // connect to our database
 
 app.set('superSecret', config.secret);
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
